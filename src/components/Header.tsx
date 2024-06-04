142a090c1a50716076f8e5ba169ec15255f67522
Julien Vaillant <julien.vaillant91@gmail.com>
2024-06-04 09:31:48 +0200

    add image and icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
-    <footer className="w-full h-11 flex justify-left p-2  font-bold text-xl">
-      Delivrou
+    <footer className="w-full flex justify-left p-2 flex-row justify-center items-center text-slate-600  font-bold text-xl">
+      <Image src="/logo.png" alt="" width={130} height={100} />
+      Commentez vos meilleures lectures !
     </footer>
   );
 };
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
+import { BookOpen, Pencil, KeyRound } from "lucide-react";
 
 const NavigationMenu = () => {
   return (
-    <menu className="p-4 flex bg-slate-600 justify-between">
+    <menu className="p-4 flex bg-slate-600 text-slate-300 justify-between">
       <li>
-        <Link href="/">Les livres</Link>
+        <Link href="/" className="flex">
+          <BookOpen className="mr-2" /> Les livres
+        </Link>
       </li>
       <li>
-        <Link href="/addReview">Déposer une critique</Link>
+        <Link href="/addReview" className="flex">
+          <Pencil className="mr-2" /> Déposer une critique
+        </Link>
       </li>
       <li>
-        <Link href="/login">Se connecter</Link>
+        <Link href="/login" className="flex">
+          <KeyRound className="mr-2" /> Se connecter
+        </Link>
       </li>
     </menu>
   );
