cde3f3c9e71c1d7c52f7af981eab8ab7eec42483
Julien Vaillant <julien.vaillant91@gmail.com>
2024-06-04 09:35:59 +0200

    add business types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+type User = {
+  id: number;
+  login: string;
+  password: string;
+  bio: string;
+};
+
+type Book = {
+  id: number;
+  title: string;
+  summary: string;
+};
+
+type Review = {
+  id: number;
+  text: string;
+};
