6af5cf933b177fe34edf07c0f8e6f29e8899ddc1
Julien Vaillant <julien.vaillant91@gmail.com>
2024-06-12 18:47:20 +0200

    update mysql package

--- src/utils/mysql.ts
+++ src/utils/mysql.ts
@@ -1,27 +1,25 @@
-import mysql, { Connection } from "mysql";
+import mysql from "serverless-mysql";
 
 export const createConnection = async () => {
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
+  const connection = await mysql({
+    library: require("mysql2"),
+    config: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT ?? "3306", 10),
+    },
   });
-
   return connection;
 };
 
-export const executeQuery = async <T, S>(
-  connection: Connection,
+export const executeQuery = async <T, V>(
+  connection: any,
   sql: string,
-  data?: S
+  data?: V
 ): Promise<T> => {
-  return new Promise((resolve, reject) => {
-    connection.query(sql, data, (errors, results) => {
-      if (errors) {
-        return reject(errors);
-      }
-      return resolve(results);
-    });
-  });
+  const queryResult = (await connection.query(sql, data)) as T;
+
+  return queryResult;
 };
