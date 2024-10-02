e9c09207a8babb1beb66da65f87c7d25dfc77bb9
Julien Vaillant <julien.vaillant91@gmail.com>
2024-10-02 15:42:25 +0200

    modify title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <footer className="w-full flex justify-left p-2 flex-row  items-center text-slate-600  font-bold text-xl">
       <Image src="/logo.png" alt="" width={90} height={90} className="mr-4" />
-      Commentez! ({process.env.NEXT_PUBLIC_ENV})
+      Commentez vos livres! ({process.env.NEXT_PUBLIC_ENV})
     </footer>
   );
 };
